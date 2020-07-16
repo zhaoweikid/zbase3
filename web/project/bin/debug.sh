93d786792a45bda1cfb3e351a93d790407eed7ef
zhaoweikid <zhaoweikid@gmail.com>
2020-07-16 21:11:11 +0800

    change

--- web/project/bin/debug.sh
+++ web/project/bin/debug.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+p1=`pwd`
+p2=${p1%zbase3*}
+export PYTHONPATH=$PYTHONPATH:$p2
 python3 server.py debug $1
 #/home/test/python/bin/watchmedo auto-restart -d . -p "*.py" /home/test/python/bin/python server.py debug $1
 #watchmedo auto-restart -d . -p "*.py" python server.py debug $1
