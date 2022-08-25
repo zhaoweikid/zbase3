667cdd4607729c6883acb336b50bb90f08ce25da
zhaoweikid <zhaoweikid@gmail.com>
2022-08-25 22:06:45 +0800

    add new_sn

--- utils/createid.py
+++ utils/createid.py
@@ -11,14 +11,16 @@ import datetime
 
 # msec(42b) + server_id(6b) + seq(16b)
 # muse have a myql conn
-def new_id64(**kwargs):
+def new_id64(conn, server_id=-1):
     ''' kwargs: conn - a mysql connection '''
-    conn = kwargs['conn']
+    #conn = kwargs['conn']
     msec = int(time.time()*1000)
     ret  = conn.get("select uuid_short()", isdict=False)
     uuid = ret[0]
     seq  = uuid % 65535;
-    return (msec << 22) + (conn.server_id << 16) + seq
+    if server_id < 0:
+        server_id = conn.server_id
+    return (msec << 22) + (server_id << 16) + seq
 
 def unpack_id64(xid):
     ''' xid - a id create by new_id64 '''
@@ -30,6 +32,23 @@ def unpack_id64_time(xid):
     ''' xid - a id create by new_id64 '''
     msec = (xid >> 22)
     return datetime.datetime.fromtimestamp(int(msec/1000.0))
+
+
+# create sequence number
+# year(4)+month(2)+day(2)+server_id(1)+seq(9)
+# muse have a myql conn
+def new_sn(conn, server_id=-1):
+    ''' kwargs: conn - a mysql connection '''
+    msec = int(time.time()*1000)
+    now  = datetime.datetime.now()
+    ret  = conn.get("select uuid_short()", isdict=False)
+    uuid = ret[0]
+    if server_id < 0:
+        server_id = conn.server_id
+    seq  = uuid % 999999999;
+    return '{:4d}{:02d}{:02d}{:1d}{:09d}'.format(now.year, now.month, now.day, server_id, seq)
+
+
 
 def test():
     from zbase3.base import logger, dbpool
